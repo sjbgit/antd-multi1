732d5289dce505bd18fd469c2eb0131fb96020d4
sjbgit <sjb_strat@hotmail.com>
2022-12-27 22:03:42 +0000

    spans for label and columns

--- src/components/Form1.jsx
+++ src/components/Form1.jsx
@@ -3,14 +3,23 @@ import { Button, Form, Input, Tabs } from 'antd';
 
 const Form1 = () => {
   return (
-    <Form name="userForm" layout='horizonal' colon={true}>
+    <Form name="userForm" layout='horizonal' colon={false}
+    labelCol={{
+      span: 4,
+    }}
+    wrapperCol={{
+      span: 16,
+    }} >
       <Form.Item label="Name" name="name">
       <Input name="name" />
       </Form.Item>
       <Form.Item label="Age" name="age">
       <Input age="age" />
       </Form.Item>
-      <Form.Item>
+      <Form.Item wrapperCol={{
+          offset: 4,
+          span: 16,
+        }}>
       <Button htmlType="submit">Submit</Button>
       </Form.Item>
       
